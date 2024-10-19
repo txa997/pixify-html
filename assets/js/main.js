2b4720cdb5ef7c3667c4ea03a89a54190545c9f2
akmol hosen <akmolhosen997@gmail.com>
2024-10-19 18:55:10 +0600

    h3-50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ gsap.config({
 
 // lenis-smooth-scroll
 const lenis = new Lenis({
-	duration: .8, 
+	duration: .6, 
 	easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
 	direction: 'vertical', 
 	smooth: true, 
